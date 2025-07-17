5120e27888fe86285e56e0ad1e62241095bcb0ab
IngridAndreaa <ingrid.pauchard@inacapmail.cl>
2025-07-17 09:59:17 -0400

    Primera actualizacion de Mi Primer React

---
+++ src/MiComponente.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MiComponente() {
+  return (
+    <div>
+      <h2>Este es mi propio componente</h2>
+      <p>Estoy aprendiendo React paso a paso.</p>
+    </div>
+  );
+}
+
+export default MiComponente;
