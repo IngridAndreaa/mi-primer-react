09bc64b20824b70b7221be01c495515f14c6909e
IngridAndreaa <ingrid.pauchard@inacapmail.cl>
2025-07-17 09:59:37 -0400

    Actualizacion

--- src/MiComponente.js
+++ src/MiComponente.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import imagen1 from './imagenes/imagen1.jpg'; // Importa la imagen
+import './MiComponente.css'; // Si quieres darle estilos
 
 function MiComponente() {
   return (
     <div>
       <h2>Este es mi propio componente</h2>
       <p>Estoy aprendiendo React paso a paso.</p>
+      <img src={imagen1} alt="Descripción de la imagen" className="imagen-mi-componente"/>
     </div>
   );
 }
 
 export default MiComponente;
+
